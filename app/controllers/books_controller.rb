7614f128e0e4cc32feffa8ab943955c821942202
sayaka-shinomura <138592145+sayaka-shinomura@users.noreply.github.com>
2023-08-05 10:25:39 +0000

    [Add] checksheet29

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,16 @@
 class BooksController < ApplicationController
+
+  #投稿画面
   def new
     @book = Book.new
+  end
+
+  #投稿データの保存
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to @book
   end
 
   def index
@@ -11,4 +21,12 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  #投稿データのストロングパラメータ
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ devise_for :users
 root to: "homes#top"
 get "homes/about" => "homes#about", as: "about"
 
-resources :books, only: [:new, :index, :show, :edit]
+resources :books, only: [:new, :create, :index, :show, :edit]
 end
