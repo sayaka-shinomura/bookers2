7f47103e93c1d95ceb3fdba0eaba74ca51196684
sayaka-shinomura <138592145+sayaka-shinomura@users.noreply.github.com>
2023-08-05 11:15:45 +0000

    [Add] checksheet18-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
 devise_for :users
 root to: "homes#top"
 get "homes/about" => "homes#about", as: "about"
 
 resources :books, only: [:new, :create, :index, :show, :edit]
+
+resources :users, only: [:index, :show, :edit]
+
 end
