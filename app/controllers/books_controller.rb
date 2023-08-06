20cd9be195598c67024a2c5ddd27422e0eb81737
sayaka-shinomura <138592145+sayaka-shinomura@users.noreply.github.com>
2023-08-06 01:18:50 +0000

    [Add] checksheet48

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,6 +25,16 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update (book_params)
+      redirect_to @book
+    else
+      render :edit
+    end
   end
 
   #投稿データのストロングパラメータ
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ devise_for :users
 root to: "homes#top"
 get "homes/about" => "homes#about", as: "about"
 
-resources :books, only: [:new, :create, :index, :show, :edit]
+resources :books, only: [:new, :create, :index, :show, :edit, :update]
 
 resources :users, only: [:index, :show, :edit, :update]
 
