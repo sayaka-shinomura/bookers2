5a3b7236484072986a8c1cf12424752177028149
sayaka-shinomura <138592145+sayaka-shinomura@users.noreply.github.com>
2023-08-09 14:09:12 +0000

    [Finish] checksheet24

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   #ログインしていない状態でアクセス
-  before_action :authenticate_user!, except: [:top]
+  before_action :authenticate_user!, except: [:top, :about]
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -17,6 +17,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:email])
   end
 end
